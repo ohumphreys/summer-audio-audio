78c115bddb66dd7f3f03fd8a40d4bad3d742d1ab
ohumphreys <onh@stanford.edu>
2026-08-26 17:21:52 -0700

    simplify experiment_id logic

--- config.js
+++ config.js
@@ -9,32 +9,24 @@ const INPUTS = Object.freeze({
 
 
 // dynamically choose the voice to be tested based on the URL
+// also the experiment id, so that it takes a counterbalance condition from the correct talker's datapipe page
 // for GA go to index.html?key=GZ9wgyFShkUvRW1k9zUDp9khx0Q8q7
 // for Southern go to index.html?key=Sc3V0X5Ex7VpbHTDrHuYnB491miEyE
 
-const VOICE_BEING_TESTED = ( () => {
-    const params = new URLSearchParams(window.location.search);
-    const key = params.get('key')
+const params = new URLSearchParams(window.location.search);
+const key = params.get('key')
 
-    // These strings are totally random besides the first letter, they are just this long to insure no one accidentally navigates from one to the other
-    if (key == 'GZ9wgyFShkUvRW1k9zUDp9khx0Q8q7') return 'GA';
-    else if (key == 'Sc3V0X5Ex7VpbHTDrHuYnB491miEyE') return 'Southern';
-    else {
-        //something has gone wrong
-        window.location.href = 'error.html';
-        return null;
-    }   
-})()
+let VOICE_BEING_TESTED, EXPERIMENT_ID;
 
+if (key == 'GZ9wgyFShkUvRW1k9zUDp9khx0Q8q7') {
+    VOICE_BEING_TESTED = 'GA'
+    EXPERIMENT_ID = 'A902rKEXAULc'
+} else if (key == 'Sc3V0X5Ex7VpbHTDrHuYnB491miEyE') {
+    VOICE_BEING_TESTED = 'Southern'
+    EXPERIMENT_ID = 'fjHxRtiCY8RF'
+} else {
+    //something has gone wrong
+    window.location.href = 'error.html';
+}
 
-// get experiment id based on voice_being_tested so that datapipe gives a condition from the right set
-const EXPERIMENT_ID = ( () => {
-    if (VOICE_BEING_TESTED == 'GA') return 'A902rKEXAULc'
-    else if (VOICE_BEING_TESTED == 'Southern') return 'fjHxRtiCY8RF'
-    else {
-        // something has gone *very* wrong
-        window.location.href = 'error.html';
-        return null;
-    }
-})()
 
